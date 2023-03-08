33bf2cf89afd8d4d3ba17a53199d19c5cf8484a8
Nayeon <89979344+Nayeon97@users.noreply.github.com>
2023-03-09 00:24:52 +0900

    Create 01.ts

---
+++ 01.ts
@@ -0,0 +1,53 @@
+export type User = unknown;
+
+export const users: unknown[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+
+export function logPerson(user: unknown) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log('Users:');
+users.forEach(logPerson);
+
+
+// solution 
+// User 객체 타입을 정의해줌.
+export interface User  {
+    name: string,
+    age: number,
+    occupation: string,
+
+};
+
+export const users: User[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+
+export function logPerson(user: User) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log('Users:');
+users.forEach(logPerson); 
+
+// 이것이 난이도 줄이기인가..싶다. 
